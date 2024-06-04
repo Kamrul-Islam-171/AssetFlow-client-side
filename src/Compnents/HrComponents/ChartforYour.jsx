20417c9018ad8a368b333eb21a66d029180e2737
Kamrul-Islam-171 <kamrul.ruet.171@gmail.com>
2024-06-04 21:00:21 +0600

    pie chart added

--- src/Compnents/HrComponents/ChartforYour.jsx
+++ src/Compnents/HrComponents/ChartforYour.jsx
@@ -1,76 +1,100 @@
+import React, { PureComponent, useContext } from 'react';
+import { PieChart, Pie, Sector, Cell, ResponsiveContainer } from 'recharts';
+import { AuthContext } from '../../Provider/AuthProvider';
+import useAxiosSecure from '../../Hooks/useAxiosSecure';
+import { useQuery } from '@tanstack/react-query';
+import LoadingSpinner from '../LoadingSpinner/LoadingSpinner';
+import { Legend } from '@headlessui/react';
 
-import React, { PureComponent } from 'react';
-import { BarChart, Bar, Rectangle, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
-const data = [
-  {
-    name: 'Page A',
-    uv: 4000,
-    pv: 2400,
-    amt: 2400,
-  },
-  {
-    name: 'Page B',
-    uv: 3000,
-    pv: 1398,
-    amt: 2210,
-  },
-  {
-    name: 'Page C',
-    uv: 2000,
-    pv: 9800,
-    amt: 2290,
-  },
-  {
-    name: 'Page D',
-    uv: 2780,
-    pv: 3908,
-    amt: 2000,
-  },
-  {
-    name: 'Page E',
-    uv: 1890,
-    pv: 4800,
-    amt: 2181,
-  },
-  {
-    name: 'Page F',
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
-  },
-  {
-    name: 'Page G',
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
-  },
-];
 
+
+const COLORS = ['#0088FE', '#00C49F'];
+
+const RADIAN = Math.PI / 180;
+const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent, index }) => {
+  const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+  const x = cx + radius * Math.cos(-midAngle * RADIAN);
+  const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
+  return (
+    <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
+      {`${(percent * 100).toFixed(0)}%`}
+    </text>
+  );
+};
 const ChartforYour = () => {
-    return (
-        <ResponsiveContainer width="100%" height="100%">
-        <BarChart
-          width={500}
-          height={300}
-          data={data}
-          margin={{
-            top: 5,
-            right: 30,
-            left: 20,
-            bottom: 5,
-          }}
-        >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="name" />
-          <YAxis />
-          <Tooltip />
-          <Legend />
-          <Bar dataKey="pv" fill="#8884d8" activeBar={<Rectangle fill="pink" stroke="blue" />} />
-          <Bar dataKey="uv" fill="#82ca9d" activeBar={<Rectangle fill="gold" stroke="purple" />} />
-        </BarChart>
+  const dataInfo = [];
+
+  const { user, loading } = useContext(AuthContext);
+  const axiosSecure = useAxiosSecure();
+
+  const { data: retunrabe = '', isLoading: isLoading1 } = useQuery({
+    queryKey: ['returnable', user?.email],
+    enabled: !loading && !!user?.email,
+    queryFn: async () => {
+      const { data } = await axiosSecure.get(`/returnable/${user?.email}`);
+      dataInfo.push({ name: 'Returnable', value: data.count })
+      return data.count
+    }
+  })
+  const { data: nonRetunrabe = '', isLoading: isLoading2 } = useQuery({
+    queryKey: ['non-returnable', user?.email],
+    enabled: !loading && !!user?.email,
+    queryFn: async () => {
+      const { data } = await axiosSecure.get(`/non-returnable/${user?.email}`);
+      dataInfo.push({ name: 'Returnable', value: data.count })
+      return data.count
+    }
+  })
+  if (loading || isLoading1 || isLoading2) return <LoadingSpinner></LoadingSpinner>
+  // const x = {name : 'Returnable', value : retunrabe};
+  // const y = {name : 'Non Returnable', value : nonRetunrabe};
+  // data.push(x);
+  // data.push(y);
+  if (retunrabe && nonRetunrabe && dataInfo.length === 0) {
+    for (let x = 1; x <= 2; x++) {
+      if (x == 1) {
+        dataInfo.push({ name: 'Returnable', value: retunrabe })
+      }
+      else {
+        dataInfo.push({ name: 'Non Returnable', value: nonRetunrabe })
+      }
+    }
+  }
+  console.log('data = ', dataInfo)
+
+
+  return (
+    <div className=' h-[300px] mt-16'>
+      {/* <p>{retunrabe}</p>
+      <p>{nonRetunrabe}</p> */}
+      <p className='text-4xl text-center'>Comparison Chart</p>
+      <ResponsiveContainer width="100%" height="100%">
+        <PieChart width={400} height={400}>
+          <Pie
+            data={dataInfo}
+            cx="50%"
+            cy="50%"
+            labelLine={false}
+            label={renderCustomizedLabel}
+            outerRadius={80}
+            fill="#8884d8"
+            dataKey="value"
+          >
+            {dataInfo.map((entry, index) => (
+              <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+            ))}
+          </Pie>
+          {/* <Legend></Legend> */}
+        </PieChart>
       </ResponsiveContainer>
-    );
+      <div className='text-center flex gap-5 justify-center'>
+        <p className='text-[#0088FE]'>Retunrable</p>
+        <p className='text-[#00C49F]'>Non Retunrabe</p>
+      </div>
+    </div>
+  );
 };
 
 export default ChartforYour;
